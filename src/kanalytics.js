d10b849628801b898e49d06ea2ac154b932797c5
yair.ansbacher <Yair.Ansbacher@kaltura.com>
2017-10-29 11:30:48 +0200

    fix(FEC-7377)

--- src/kanalytics.js
+++ src/kanalytics.js
@@ -205,7 +205,7 @@ export default class KAnalytics extends BasePlugin {
       uiConfId: this.config.uiConfId || 0,
       partnerId: this.config.partnerId,
       widgetId: this.config.partnerId ? "_" + this.config.partnerId : "",
-      referrer: document.referrer
+      referrer: document.referrer || document.URL
     };
   }
 
