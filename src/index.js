43d0770bbcbac081a900938c322c08ac8c9c0a27
Oren Me <oren.me@gmail.com>
2017-11-08 15:43:05 +0200

    chore: export plugin via index file (#25)
    Move the exported data and plugin registration process to an index file

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// @flow
+import {registerPlugin} from 'playkit-js'
+import KAnalytics from './kanalytics'
+
+declare var __VERSION__: string;
+declare var __NAME__: string;
+
+export default KAnalytics;
+export {__VERSION__ as VERSION, __NAME__ as NAME};
+
+const pluginName = "kanalytics";
+/**
+ * Register the plugin in the playkit-js plugin framework.
+ */
+registerPlugin(pluginName, KAnalytics);
